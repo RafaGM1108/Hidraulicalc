6adfb270804e774fbb2cad47d68ea0cd345e6ffc
Rafael Garcia <114042129+RafaGM1108@users.noreply.github.com>
2023-03-11 12:26:24 -0500

    Add files via upload

--- Project_Rafael.py
+++ Project_Rafael.py
@@ -3,29 +3,72 @@ import numpy as np
 
 csfont = {'fontname':'Times New Roman'}
 
+st.title("Bienvenido!")
 #--------------------Pilot Part-------------------#
 
-dominios=st.sidebar.radio("Proyecto 1 - Hidraulica",("Caudal","Perdidas",),key=1) 
-if dominios=="Caudal":
-    listaFunc=[" ","función (1)", "función (2)", "función (3)", "función (4)", "función (5)"]
-    Deltas=0.01
-    funcion=st.sidebar.selectbox("Menú",listaFunc, key=5)
-    if funcion==" ":
-        st.title("Bienvenido!")
-        st.write("En esta sección se realizaran los calculos correspondientes a un caudal ")
- 
-elif dominios=="Perdidas":
-    listaFunc=[" ","Señal (1)", "Señal (2)", "Señal (3)"]
-    listaFunc1=[" ","Portadora (1)", "Portadora (2)", "Portadora (3)"]
-    Deltas=0.01
-    funcion=st.sidebar.selectbox("Menú",listaFunc, key=6)
-    if funcion==" ":
-        st.title("Bienvenido!")
-        st.write("En esta sección se realizaron los calculos de perdidas")
-    elif funcion=="Señal (1)" or funcion=="Señal (2)" or funcion=="Señal (3)":
-        funcionp1=st.sidebar.selectbox("Menú",listaFunc1, key=40)
-        if funcion==" ":
-            st.title("Bienvenido!")
-            st.write("Por favor seleccione un par de funciones del menú para comenzar con la representacion de señales con series de Fourier")
+def Perdidas():
+    k=st.number_input("Digite el numero de armonicos con los que desea reconstruir la Señal") 
+    Q=st.number_input("Digite el valor del caudal (m^3/s):\n")
+    L=st.number_input("Digite el valor de la longitud (m):\n")
+    D=st.number_input("Digite el valor del diametro (m):\n")
+    ks=st.number_input("Digite el valor de la rugosidad del material (m):\n")
+    u=st.number_input("Digite el valor de la viscosidad (m^2/s):\n")
+    E=st.number_input(input("Digite el valor del Error:\n"))
+    A=np.pi*(D**2)/4
+    V=Q/A
+    Re=(V*D)/u
+    fi=float(input("Digite el valor de fi:\n"))
+    def i(fi):
+        fi1=(1/(-2*np.log10((ks/(3.7*D)+2.51/(Re*np.sqrt(fi))))))**2
+        return fi1
+    fi1=i(fi)
+    DE=abs(fi-fi1)
+    while E<DE:
+        temp=fi1
+        fi=temp
+        fi1=i(temp)
+        DE=abs(fi-fi1)
+    print(DE)
+    Hf=(fi1*L*(V**2))/(2*D*9.81)
+    print("El valor del coeficiente de fricción f es: ",fi1)
+    print("El valor de la perdida de cabeza por friccion es:",Hf)
+    
+def Caudal():
+    l=float(input("Digite el valor de la longitud (m):\n"))
+    d=float(input("Digite el valor del diametro (m):\n"))
+    ks=float(input("Digite el valor de la rugosidad del material (m):\n"))
+    μ=float(input("Digite el valor de la viscosidad (pa*s):\n"))
+    E=float(input("Digite el valor del Error:\n"))
+    km=float(input("Digite el valor de las perdidas menores (m):\n"))
+    ρ=float(input("Digite el valor de la densidad del material (kg/m^3):\n"))
+    z2=float(input("Digite el valor de z2 (m):\n"))
+    H=float(input("Digite el valor de la altura total (m):\n"))
+
+    v=μ/ρ #viscosidad
+    hf=H
+    ε=ks/d
+
+    Vi=(-2*np.sqrt(2*9.81*d*hf/l))*np.log10((ks/(3.7*d))+((2.51*v*np.sqrt(l))/(d*np.sqrt(2*9.81*d*hf))))
+    hfi=H-z2-km*(Vi**2)/(2*9.81)
+    
+    def i(hf):
+        Vi1=(-2*np.sqrt(2*9.81*d*hf/l))*np.log10((ks/(3.7*d))+((2.51*v*np.sqrt(l))/(d*np.sqrt(2*9.81*d*hf))))
+        hf1=H-z2-km*(Vi1**2)/(2*9.81)
+        return (hf1,Vi1)
+
+    DE=abs(hf-hfi)
+    while E<DE:
+        temp1=hfi
+        iteracion=i(temp1)    
+        hfi=iteracion[0]
+        DE=abs(hf-hfi)
+
+
+dominios=st.sidebar.radio("Proyecto 1 - Hidraulica",("Perdidas","Caudal",),key=1) 
+if dominios=="Perdidas":
+    Caudal()
+
+elif dominios=="Caudal":
+    Perdidas()
 
 
