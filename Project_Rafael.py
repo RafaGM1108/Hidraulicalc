efd20b1162395d943dc2cd56b7d0dcedfecf7fd6
Rafael Garcia <114042129+RafaGM1108@users.noreply.github.com>
2023-03-11 20:51:21 -0500

    Add files via upload

--- Project_Rafael.py
+++ Project_Rafael.py
@@ -6,16 +6,16 @@ csfont = {'fontname':'Times New Roman'}
 #--------------------Pilot Part-------------------#
 
 def Perdidas():
-    Q=st.number_input("Digite el valor del caudal (m^3/s):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    L=st.number_input("Digite el valor de la longitud (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    D=st.number_input("Digite el valor del diametro (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    ks=st.number_input("Digite el valor de la rugosidad del material (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    u=st.number_input("Digite el valor de la viscosidad (m^2/s):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    E=st.number_input("Digite el valor del Error:\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
+    Q=st.number_input("Digite el valor del caudal (m^3/s):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
+    L=st.number_input("Digite el valor de la longitud (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
+    D=st.number_input("Digite el valor del diametro (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
+    ks=st.number_input("Digite el valor de la rugosidad del material (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
+    u=st.number_input("Digite el valor de la viscosidad (m^2/s):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
+    E=st.number_input("Digite el valor del Error:\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
     A=np.pi*(D**2)/4
     V=Q/A
     Re=(V*D)/u
-    fi=st.number_input("Digite el valor de fi:\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
+    fi=st.number_input("Digite el valor de fi:\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.7f")
     def i(fi):
         fi1=(1/(-2*np.log10((ks/(3.7*D)+2.51/(Re*np.sqrt(fi))))))**2
         return fi1
@@ -31,8 +31,8 @@ def Perdidas():
     st.write("El valor de la perdida de cabeza por friccion es:",Hf)
     
 def Caudal():
-    l=st.number_input("Digite el valor de la longitud (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
-    d=st.number_input("Digite el valor del diametro (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
+    l=st.number_input("Digite el valor de la longitud (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.5f")
+    d=st.number_input("Digite el valor del diametro (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.5f")
     ks=st.number_input("Digite el valor de la rugosidad del material (m):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
     μ=st.number_input("Digite el valor de la viscosidad (pa*s):\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
     E=st.number_input("Digite el valor del Error:\n", min_value=1e-8, max_value=1e8, value=1.00000, step=0.00001, format="%.10f")
@@ -44,6 +44,7 @@ def Caudal():
     v=μ/ρ #viscosidad
     hf=H
     ε=ks/d
+    A=np.pi*(d**2)/4
 
     Vi=(-2*np.sqrt(2*9.81*d*hf/l))*np.log10((ks/(3.7*d))+((2.51*v*np.sqrt(l))/(d*np.sqrt(2*9.81*d*hf))))
     hfi=H-z2-km*(Vi**2)/(2*9.81)
@@ -58,7 +59,10 @@ def Caudal():
         temp1=hfi
         iteracion=i(temp1)    
         hfi=iteracion[0]
+        Q=iteracion[1]*A
         DE=abs(hf-hfi)
+    Cdl=Q
+    st.write("El valor del caudal es:",Cdl)
 
 
 dominios=st.sidebar.radio("Proyecto 1 - Hidraulica",("Perdidas","Caudal","Viscosidad","Diametro","Potencia",),key=1) 
